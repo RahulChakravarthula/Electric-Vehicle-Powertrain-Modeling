05104be848a0e735d7dd67e99bd7ad0c21e700fd
RahulChakravarthula <rahul.chakravarthula@yahoo.com>
2025-05-11 17:33:35 +0200

    Add files via upload

---
+++ EfficiencySensitivityAnalysisParameters.m
@@ -0,0 +1,11 @@
+% Parameters of powertrain 
+
+Tr = 0.33; % Tire radius
+GR = 6; % Gear ratio
+mu_gb = 0.95; % Gearbox Efficiency
+phi = 0 ; % Power factor angle
+R_thjc = 0.51; % Junction-to-Case thermal resistance
+R_thcb = 0.02; % Case-to-Baseplate thermal resistance
+R_thbh = 0.04; % Baseplate-to-Heatsink resistance
+R_thsa = 0.05; % Heatsink-to-Coolant resistance 
+Tinit = 40; % Intial Inverter Temperature
